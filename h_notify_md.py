429f8c45eb318e20bd2478c4ae079bed09f77439
Evgeny Frolov <evfro@live.ru>
2021-01-29 22:10:23 +0300

    enable groupnig annotations by source

--- h_notify_md.py
+++ h_notify_md.py
@@ -1,12 +1,16 @@
 import os, sys
 sys.path.append("./hypothesis") # make submodule "visible"
+from collections import defaultdict
 import dateutil.parser
 import logging
 from mdutils.mdutils import MdUtils
 from h_notify import Notifier
 
-date_format = "%Y-%m-%d"
-file_log_err_level = logging.ERROR # logging.ERROR is recommended
+# file-logging
+log_file = 'h_notify.log'
+file_date_format = "%Y-%m-%d"
+note_date_format = "%d-%m-%Y %H:%M"
+file_log_err_level = logging.DEBUG # logging.ERROR is recommended
 
 
 def init_logger(name=None, level=None, custom_logging=None):
@@ -15,8 +19,6 @@ def init_logger(name=None, level=None, custom_logging=None):
         logging.addLevelName(custom_logging, 'H_NOTIFY')
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
-    # file-logging
-    log_file = 'h_notify.log'
     fh = logging.FileHandler(log_file)
     if level is None:
         level = file_log_err_level
@@ -34,25 +36,63 @@ class MarkdownNotifier(Notifier):
         self.file_name = file_name
         self.logger = init_logger(name='h_notify.MarkdownNotifier')
 
-    def notify(self, anno=None, groupname=None, folder=None):
-        try:
+    def notify(self, anno_list, groupname=None, folder=None):
+        notes = defaultdict(lambda: defaultdict(list))
+        for anno in anno_list:
             vars = self.make_vars(anno, groupname)
             dt = dateutil.parser.parse(anno.updated)
             if not self.file_name:
-                self.file_name = dt.strftime(date_format)
+                self.file_name = dt.strftime(file_date_format)
             file_path = os.path.join(self.folder, self.file_name)
-            self.generate_markdown(
-                file_path,
-                anno.uri,
-                vars["anno_url"],
-                anno.doc_title,
-                vars["quote"],
-                anno.text,
-                vars["tags"],
-                dt,
+            note = notes[(file_path, anno.doc_title)]
+            note["source"].append(anno.uri)
+            note["location"].append(vars["anno_url"])
+            note["note"].append(anno.text)
+            note["quote"].append(vars["quote"])
+            note["tags"].append(vars["tags"])
+            note["timestamp"].append(dt.strftime(note_date_format))
+        self.generate_markdown(notes)
+
+    def generate_markdown(self, notes):
+        for (file_path, title), body in notes.items():
+            mdFile = MdUtils(file_path, title="")
+            mdFile.new_header(
+                level=1, title=title, style="atx", add_table_of_contents="n"
             )
-        except:
-            self.logger.exception(f'Failed on id: {anno.id}, target: {anno.uri}')
+            self.markdown_from_dict(mdFile, body)
+            mdFile.new_paragraph("\n---\n")
+            self.write_markdown(mdFile)
+
+    def markdown_from_dict(self, mdFile, body):
+        head_source = body['source'][0]
+        mdFile.new_line(f"[source]({head_source})")
+
+        for i, source in enumerate(body['source']):
+            tags = body['tags'][i]
+            note = body['note'][i]
+            quote = body['quote'][i]
+            location = body['location'][i]
+            timestamp = body['timestamp'][i]
+
+            if source != head_source:
+                mdFile.new_paragraph(f"[source]({source})")
+            else:
+                mdFile.new_paragraph("")
+                
+            if tags:
+                tags = ' '.join([f'#{tag.strip()}' for tag in tags.split(',')])
+                mdFile.new_line(tags)
+            
+            indent = ''
+            if note:
+                note = "\n".join([
+                    f"- {line}" if not line.strip().startswith('- ') else line
+                    for line in note.split('\n') if line.strip()
+                ])
+                mdFile.new_paragraph(note)
+                indent = '\t'
+            mdFile.new_line(f"{indent}> {quote}")
+            mdFile.new_line(f"{indent}> [{timestamp}]({location})")
 
     def write_markdown(self, md_object):
         file_name = md_object.file_name
@@ -64,28 +104,6 @@ class MarkdownNotifier(Notifier):
                 + md_object.reference.get_references_as_markdown()
             )
             file_object.write(text)
-
-    def generate_markdown(
-        self, file_path, source, location, topic, quote, note, tags, timestamp
-    ):
-        mdFile = MdUtils(file_path, title="")
-        mdFile.new_header(
-            level=2, title=f"{topic}", style="atx", add_table_of_contents="n"
-        )
-        if tags:
-            tags = ' '.join(['#'+tag.strip() for tag in tags.split(',')])
-            mdFile.new_line(f"{tags}")
-        mdFile.new_paragraph(f"> {quote}")
-        mdFile.new_line(f"> [{timestamp}]({location})")
-        if note:
-            mdFile.new_paragraph("Notes:", bold_italics_code="bi")
-            mdFile.new_paragraph(note)
-        mdFile.new_paragraph(f"[source]({source})")
-        mdFile.new_paragraph("\n---\n")
-        self.write_markdown(mdFile)
-
-
-# Local markdown recipes
 
 
 def notify_markdown_group_activity(
@@ -105,5 +123,5 @@ def notify_markdown_group_activity(
         pickle=pickle,
         notified_ids=notified_ids,
     )
-    notifier.notify_facet(facet="group", value=group, groupname=groupname)
+    notifier.notify_facet(facet="group", value=group, groupname=groupname, gather=True)
     return notified_ids
